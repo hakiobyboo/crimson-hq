9ba724a4444e2a9bfe2cef63e927573b4e690a2e
hakiobyboo <tristanniot.pro@gmail.com>
2026-02-01 17:24:20 +0100

    Update logic.py

--- logic.py
+++ logic.py
@@ -41,6 +41,8 @@ def show_intel():
     players = [
         {"label": "Boo ツ", "n": "Boo%20%E3%83%84", "t": "1tpas"}, 
         {"label": "Kuraime", "n": "kuraime", "t": "ezz"}
+        {"label": "turboS", "n": "turboS", "t": "SPEED"}
+        {"label": "Nef", "n": "Nef", "t": "SPK"}
     ]
     cols = st.columns(2)
     for i, pl in enumerate(players):
@@ -273,5 +275,6 @@ def show_strategy_map(current_map):
                             if st.button("🗑️", key=f"del_{side}_{idx}"):
                                 os.remove(f"{path}/{f}")
                                 st.rerun()
+
 
 
